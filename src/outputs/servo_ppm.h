711ec543b420cb22ca75411ab4a5839464458eba
chris <chrissteffen98@gmail.com>
2021-01-19 17:30:46 +0100

    Implemented most of PPMChannel class

--- src/outputs/servo_ppm.h
+++ src/outputs/servo_ppm.h
@@ -5,40 +5,70 @@
 
 #include "Arduino.h"
 
-#include "utils/interval_control.h"
 #include "utils/device_status.h"
 
 
 
+#define MAX_NUM_CHANNELS 20
+
+
 #define STANDARD_LOW 1000
 #define STANDARD_HIGH 2000
+
+#define ONESHOT125_LOW 125
+#define ONESHOT125_HIGH 250
+
+#define ONESHOT42_LOW 42
+#define ONESHOT125_HIGH 82
+
+#define MULTISHOT_LOW 5
+#define MULTISHOT_HIGH 25
 
 
 
 class PPMChannel {
 public:
 
-    enum PROTOCOL {
-        PROTOCOL_STANDARD,
-        PROTOCOL_ONESHOT125,
-        PROTOCOL_MULTISHOT
-    };
+    PPMChannel(int16_t pin = -1);
+    ~PPMChannel();
 
-    static void deviceThread();
+    void deviceThread();
 
     DeviceStatus getDeviceStatus();
 
-    void setPos();
+    bool setChannel(float percent);
+    float getChannel();
 
-    float getPos();
+    bool setHighTimeMaxMicros(uint32_t maxTimeUS);
+    void setHighTimeMinMicros(uint32_t minTimeUS);
+
+    uint32_t getHighTimeMaxMicros();
+    uint32_t getHighTimeMinMicros();
+
+    bool startSending();
+    void stopSending();
+
+    bool setPin(int16_t pin);
 
 private:
 
-    static IntervalControl servoInterval;
+    static int16_t usedPins[MAX_NUM_CHANNELS];
+    static byte devices;
 
-    byte _protocol = PROTOCOL_STANDARD;
+    int16_t _pin = -1;  
 
-    DeviceStatus servoStatus = DeviceStatus::DEVICE_NOT_STARTED;
+    DeviceStatus _servoStatus = DeviceStatus::DEVICE_NOT_STARTED;
+
+    float _percent = 0.5f;
+
+    uint32_t _highMaxUS = 2000;
+    uint32_t _highMinUS = 1000;
+
+    float _deltaHighUS = _highMaxUS - _highMinUS;
+    float _periodUS = _highMaxUS*1.25f;
+
+    const byte resolution = 12;
+    const uint32_t bits = (uint32_t)1<<resolution;
     
 };
 

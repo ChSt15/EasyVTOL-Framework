102eb599e943bd59fc1057f7b2a9ac2bf93b4521
chris <chrissteffen98@gmail.com>
2021-01-29 21:38:40 +0100

    first implementation of statistics helper

--- src/utils/statistics.h
+++ src/utils/statistics.h
@@ -8,16 +8,47 @@
 #include "CircularBuffer.h"
 
 
-template<typename T, size_t S>
+template<typename T, size_t S> // <sample type, sample size>
 class Statistics {
 public:
 
-    
+    void addSample(T newSample) {
+        _data.unshift(newSample);
+    }
+
+    bool updateValues() {
+
+        if (_data.size() == 0) return false;
+
+        _average = 0;
+        for (uint32_t n = 0; n < _data.size(); n++) {
+            _average += _data[n];
+        }
+        _average /= _data.size();
+
+        if (_data.size() <= 1) return false;
+
+        _deviation = 0;
+        for (uint32_t n = 0; n < _data.size(); n++) {
+            T x = _data[n] - _average;
+            __deviation += x*x;
+        }
+        _deviation = sqrt(_deviation/(_data.size() - 1));
+
+        return true;
+
+    }
+
+    T getAverage() {return _average;}
+    T getDeviation() {return _deviation;}
 
 
 private:
 
     CircularBuffer <T, S> _data;
+
+    T _average;
+    T _deviation;
 
 
 };

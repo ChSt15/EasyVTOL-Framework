e58a37e198122ed69d32b899e3f2d6e1b61d24cb
chris <chrissteffen98@gmail.com>
2021-02-03 12:42:56 +0100

    Added data members

--- src/vehicle/inertial_data.h
+++ src/vehicle/inertial_data.h
@@ -4,15 +4,21 @@
 
 #include "definitions.h"
 
+#include "vector_math.h"
+#include "quaternion_math.h"
 
 
 struct InertialData {
 
+    Vector _position;
+    Vector _velocity;
+    Vector _acceleration;
+    Vector _linearAcceleration;
 
+    Quaternion _attitude = Quaternion(1,0,0,0);
+    Vector _angularRate;
     
 };
-
-
 
 
 

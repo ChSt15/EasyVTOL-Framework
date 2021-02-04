6c04bfa96ce1ea43c8d3edd9aac1309282423413
chris <chrissteffen98@gmail.com>
2021-02-04 20:00:34 +0100

    Big structure change

---
+++ src/vehicle/starship/control.h
@@ -0,0 +1,28 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+
+
+#include "Arduino.h"
+
+
+
+
+class Control {
+public:
+
+    
+    
+
+private:
+
+
+
+    
+};
+
+
+
+
+
+#endif
---
+++ src/vehicle/starship/navigation.h
@@ -0,0 +1,89 @@
+#ifndef NAVIGATION_H
+#define NAVIGATION_H
+
+
+
+/**
+ * This is where the vehicle sensor measurements (sensorfusion) is done. This takes
+ * the sensor measurements and calculates all inertial parameters of the vehicle.
+ * This allows the navigation to be written in a general way for the simulator.
+ * Because this class will be augmented by the simulator, this structure also allows for very
+ * easy testing of the vehicle code in the simulator in a very "drag and drop" type way.
+*/
+
+
+
+#include "Arduino.h"
+
+#include "sensors/imu.h"
+
+#include "utils/interval_control.h"
+#include "utils/high_pass_filter.h"
+#include "utils/low_pass_filter.h"
+
+
+#define LOOP_RATE_LIMIT 2000
+
+
+class Navigation {
+public:
+
+    void resetInertialTEST() {_position = Vector(0,0,0); _velocity = Vector(0,0,0);}; //To be removed or replaced. This is only for testing Inertial navigation
+
+    Quaternion getAttitude() {return _attitude;}
+    Vector getAngularRate() {return _angularRate;}
+
+    Vector getPosition() {return _position;}
+    Vector getVelocity() {return _velocity;}
+    Vector getAcceleration() {return _acceleration;}
+    Vector getLinearAcceleration() {return _linearAcceleration;}
+
+
+protected:
+
+    void sensorFusionThread();
+
+
+private:
+
+    //System inertial information
+    Vector _position;
+    Vector _velocity;
+    Vector _acceleration;
+    Vector _linearAcceleration;
+
+    Quaternion _attitude = Quaternion(Vector(1,1,1), 0*DEGREES);
+    Vector _angularRate;
+
+    //Filter data
+    //HighPassFilter<Vector> gyroHPF = HighPassFilter<Vector>(0.01);
+
+    uint32_t lastGyroTimestamp = 0;
+    uint32_t lastAccelTimestamp = 0;
+    uint32_t lastMagTimestamp = 0;
+
+    //System information flagges
+    bool _angularRateValid = false;
+    bool _attitudeValid = false;
+    bool _headingValid = false;
+
+    bool _accelerationValid = false;
+    bool _velocityValid = false;
+    bool _positionValid = false;
+
+    bool _highPrecisionValid = false;
+
+    bool gyroInitialized = false;
+    bool accelInitialized = false;
+    bool magInitialized = false;
+
+    //Loop interval control
+    IntervalControl interval = IntervalControl(LOOP_RATE_LIMIT);
+
+
+};
+
+
+
+
+#endif

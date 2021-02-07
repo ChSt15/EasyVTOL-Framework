03607410da32e6c10465864302dba743dd638b9a
chris <chrissteffen98@gmail.com>
2021-02-07 16:23:01 +0100

    Added flight_modes enum for flight mode changing

---
+++ src/vehicle/starship/flight_modes.h
@@ -0,0 +1,17 @@
+#ifndef FLIGHT_MODES_H
+#define FLIGHT_MODES_H
+
+
+enum FLIGHT_MODE {
+    DISARMED,
+    ARMED,
+    HOVER,
+    BELLY_FLOP,
+    FAILSAFE,
+    TEST_TVC,
+    TEST_BELLY_FLOP
+};
+
+
+
+#endif

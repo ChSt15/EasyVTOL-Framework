cfdcbfd826cde1646b714be978808334b087da78
chris <chrissteffen98@gmail.com>
2021-02-24 22:28:03 +0100

    Needed .cpp for static class members

---
+++ src/guidance_modules/guidance_template.cpp
@@ -0,0 +1,14 @@
+#include "guidance_template.h"
+
+
+
+/**
+ * Absolute waste of space. 
+ * I hate doing this but the compiler screams when you dont.
+ * Need this for static class members. Cant do it after the class
+ * definition, must be in a .cpp file. *facepalm*
+ */
+
+
+
+ControlData Guidance::_vehicleControlSettings;

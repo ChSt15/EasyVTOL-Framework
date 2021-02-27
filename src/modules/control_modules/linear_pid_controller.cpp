27b5c0f070503e5205cbcd2c254f45bbcfaa9505
chris <chrissteffen98@gmail.com>
2021-02-27 20:53:00 +0100

    Added simple linear pid controller

---
+++ src/modules/control_modules/linear_pid_controller.cpp
@@ -0,0 +1,18 @@
+#include "linear_pid_controller.h"
+
+
+
+
+void LinearPIDController::thread() {
+
+    if(!_interval.isTimeToRun()) return; //Check if its time to run
+
+    
+
+}
+
+
+
+void LinearPIDController::init() {
+
+}

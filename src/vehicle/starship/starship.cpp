2b03da87f5ec5582cdc9f6dbea0f76c11fd154d2
chris <chrissteffen98@gmail.com>
2021-03-01 08:02:11 +0100

    Commented modules that are currently not used out.

--- src/vehicle/starship/starship.cpp
+++ src/vehicle/starship/starship.cpp
@@ -14,11 +14,11 @@ void Starship::thread() {
         //_guidanceFBW.setAngularRate(Vector(cos((float)millis()/1000.0f),0,0));
     }
 
-    _guidance->thread(); //Run guidance thread
+    //_guidance->thread(); //Run guidance thread
 
-    _control->thread(); //Run control thread
+    //_control->thread(); //Run control thread
 
-    _dynamics->thread(); //Run dynamics thread
+   // _dynamics->thread(); //Run dynamics thread
 
 }
 
@@ -26,17 +26,17 @@ void Starship::thread() {
 void Starship::init() {
 
     //link module data together
-    _control->linkControlSetpointPointer(_guidance->getControlSetpointPointer()); //Guidance -> Control
+    /*_control->linkControlSetpointPointer(_guidance->getControlSetpointPointer()); //Guidance -> Control
     _control->linkNavigationDataPointer(_navigation->getNavigationDataPointer()); //Navigation -> Control
     _dynamics->linkKinematicSetpointPointer(_control->getKinematicOutputPointer()); //Navigation -> Dynamics
     _dynamics->linkNavigationDataPointer(_navigation->getNavigationDataPointer()); //Control -> Dynamics
     //_output->linkNavigationDataPointer(_navigation->getNavigationDataPointer());
-    //_output->linkDynamicSetpointPointer(_dynamics->getDynamicSetpointPointer());
+    //_output->linkDynamicSetpointPointer(_dynamics->getDynamicSetpointPointer());*/
 
     //Initialise all modules
     _navigation->init();
     _guidance->init();
-    _control->init();
+    //_control->init();
     _dynamics->init();
     //_output->init();
 

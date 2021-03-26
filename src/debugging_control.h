86f016388de8490e061254fbc7849edffe8f8cde
chris <chrissteffen98@gmail.com>
2021-03-26 13:26:37 +0100

    Added file for debugging

---
+++ src/debugging_control.h
@@ -0,0 +1,38 @@
+#ifndef SYSTEM_MONITOR_H
+#define SYSTEM_MONITOR_H
+
+
+
+#include "definitions.h"
+
+#include "sensors/imu.h"
+#include "sensors/air_data.h"
+#include "sensors/gps.h"
+
+#include "comms/lora_2_4.h"
+
+#include "outputs/rgb_led.h"
+
+#include "kraft_kontrol.h"
+
+#include "interval_control.h"
+
+
+
+void threadSystemMonitor() {
+
+    static IntervalControl intervalControl(10);
+
+    if (!intervalControl.isTimeToRun()) return; //Only run every 100ms (10Hz)
+
+    if (Serial.available()) {
+        
+
+        
+    }
+
+}
+
+
+
+#endif

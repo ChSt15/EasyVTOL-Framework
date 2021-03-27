4642e6a48b3b783622ea6cf5cb13150cce4d6f18
chris <chrissteffen98@gmail.com>
2021-03-27 19:04:38 +0100

    File name change update and added actuator mapping

--- src/vehicle/starship/starship_dynamics.h
+++ src/vehicle/starship/starship_dynamics.h
@@ -11,7 +11,7 @@
 
 #include "definitions.h"
 
-#include "starship_output_pins.h"
+#include "starship_properties.h"
 
 #include "outputs/rgb_led.h"
 
@@ -28,9 +28,22 @@
 #include "data_containers/dynamic_data.h"
 
 
-#define FLAP_MAX_VELOCITY 30
+//Flap servo accel and velocity maximums
+#define FLAP_MAX_VELOCITY 5
 #define TOPFLAP_MAX_ACCEL 30
-#define BOTTOMFLAP_MAX_ACCEL 20
+#define BOTTOMFLAP_MAX_ACCEL 30
+
+//Actuator mapping for manual control
+#define STARSHIP_ACTUATOR_FLAPUL 0
+#define STARSHIP_ACTUATOR_FLAPUR 1
+#define STARSHIP_ACTUATOR_FLAPDR 2
+#define STARSHIP_ACTUATOR_FLAPDL 3
+#define STARSHIP_ACTUATOR_TVC1 4
+#define STARSHIP_ACTUATOR_TVC2 5
+#define STARSHIP_ACTUATOR_TVC3 6
+#define STARSHIP_ACTUATOR_TVC4 7
+#define STARSHIP_ACTUATOR_MOTORCW 8
+#define STARSHIP_ACTUATOR_MOTORCCW 9
 
 
 
@@ -65,24 +78,35 @@ private:
     //TVC servo in Y-
     PPMChannel _TVCServo4 = PPMChannel(TVC_SERVO_PIN_4, PPM_PROTOCOL::STANDARD_1000, -0.1, 1);
     //CW motor control.
-    PPMChannel _motorCW = PPMChannel(MOTOR_PIN_CW, PPM_PROTOCOL::MULTISHOT, -1, 2);
+    PPMChannel _motorCW = PPMChannel(MOTOR_PIN_CW, PPM_PROTOCOL::ONESHOT_125, -1, 2);
     //CCW motor control.
-    PPMChannel _motorCCW = PPMChannel(MOTOR_PIN_CCW, PPM_PROTOCOL::MULTISHOT, -1, 2);
+    PPMChannel _motorCCW = PPMChannel(MOTOR_PIN_CCW, PPM_PROTOCOL::ONESHOT_125, -1, 2);
     //Up Left flap servo.
     PPMChannel _flapUL = PPMChannel(FLAP_SERVO_PIN_UL, PPM_PROTOCOL::STANDARD_1000, 1, -1);
-    ServoDynamics _flapULControl = ServoDynamics(&_flapUL, 0, FLAP_MAX_VELOCITY, TOPFLAP_MAX_ACCEL);
+    ServoDynamics _flapULControl = ServoDynamics(&_flapUL, 1*DEGREES, FLAP_MAX_VELOCITY, TOPFLAP_MAX_ACCEL);
     //Up Right flap servo.
     PPMChannel _flapUR = PPMChannel(FLAP_SERVO_PIN_UR, PPM_PROTOCOL::STANDARD_1000, -1, 1);
-    ServoDynamics _flapURControl = ServoDynamics(&_flapUR, 0, FLAP_MAX_VELOCITY, TOPFLAP_MAX_ACCEL);
+    ServoDynamics _flapURControl = ServoDynamics(&_flapUR, -5*DEGREES, FLAP_MAX_VELOCITY, TOPFLAP_MAX_ACCEL);
     //Down Left flap servo.
     PPMChannel _flapDL = PPMChannel(FLAP_SERVO_PIN_DL, PPM_PROTOCOL::STANDARD_1000, -1, 1);
-    ServoDynamics _flapDLControl = ServoDynamics(&_flapDL, 0, FLAP_MAX_VELOCITY, BOTTOMFLAP_MAX_ACCEL);
+    ServoDynamics _flapDLControl = ServoDynamics(&_flapDL, -7*DEGREES, FLAP_MAX_VELOCITY, BOTTOMFLAP_MAX_ACCEL);
     //Down Right flap servo.
     PPMChannel _flapDR = PPMChannel(FLAP_SERVO_PIN_DR, PPM_PROTOCOL::STANDARD_1000, 1, -1);
-    ServoDynamics _flapDRControl = ServoDynamics(&_flapDR, 2*DEGREES, FLAP_MAX_VELOCITY, BOTTOMFLAP_MAX_ACCEL);
+    ServoDynamics _flapDRControl = ServoDynamics(&_flapDR, 1*DEGREES, FLAP_MAX_VELOCITY, BOTTOMFLAP_MAX_ACCEL);
 
     //Controls loop rate
     IntervalControl _interval = IntervalControl(1000);
+
+    //Enum for flap testing
+    enum FLAP_TEST_STAGE {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT
+    };
+
+    FLAP_TEST_STAGE _flapTestStage = FLAP_TEST_STAGE::TOP_LEFT;
+    uint8_t _flapTestCounter = 0;
 
 
     void _getTVCAngles(const Vector &direction, const float &twist, float &tvc1, float &tvc2, float &tvc3, float &tvc4);

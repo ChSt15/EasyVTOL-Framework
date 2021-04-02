138610dfa367aea4cd0ab47b6cb7e47a26c0c828
ChSt98 <chrissteffen98@gmail.com>
2021-04-02 21:06:05 +0200

    Added connection header

---
+++ src/connections/starship_v_1_0.h
@@ -0,0 +1,19 @@
+#ifndef STARSHIP_V_1_0_H
+#define STARSHIP_V_1_0_H
+
+/**
+ * 
+ * This defines all connection pins of servos, motors, sensors etc.
+ * This could of course be different from vehicle to vehicle but IMU 
+ * and LoRa radios could be the some depending on the board used.
+ * 
+*/
+
+#include "boards/board_v_1_0.h"
+
+
+#define FLAP_UL_SERVO_PIN 
+
+
+
+#endif

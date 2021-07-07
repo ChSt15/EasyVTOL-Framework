b2d60dd85c6c9931eee8910f6c1d07804a2788fc
ChSt98 <chrissteffen98@gmail.com>
2021-07-07 13:50:57 +0200

    Added definition to statics

---
+++ src/modules/module_abstract.cpp
@@ -0,0 +1,7 @@
+#include "KraftKontrol/modules/module_abstract.h"
+
+
+
+Topic<KraftMessageContainer> Module_Abstract::globalMessages_;// = Topic<KraftMessageContainer>();
+
+List<Module_Abstract*> Module_Abstract::existingModules_;

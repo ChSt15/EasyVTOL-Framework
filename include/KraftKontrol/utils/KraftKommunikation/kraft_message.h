08fef6149d5b4700a6b091b660240e05f115a91b
ChSt98 <chrissteffen98@gmail.com>
2021-07-07 13:51:31 +0200

    Large changes to message structure

--- include/KraftKontrol/utils/KraftKommunikation/kraft_message.h
+++ include/KraftKontrol/utils/KraftKommunikation/kraft_message.h
@@ -9,41 +9,74 @@
 
 
 
-//Indicates the type of packet. Use eKraftMessageType_StandardEnd_ID and up for custom Packets
-enum eKraftMessageType_t : uint8_t {
+/**
+ * Indicates the type of message. 
+ * This is to differentiate between telemetry, telecommands, general data etc.
+ * Use eKraftMessageType_End_ID to extend the enum. MUST BE AT END OF ENUM!
+ */
+enum eKraftMessageType_t: uint32_t {
     //Used for initialising and signify that it was never set.
     eKraftMessageType_Invalid_ID,
-    //Simply signals that connection is still there. Should be send at 1Hz rate.
-    eKraftMessageType_Heartbeat_ID,
-    //To be returned to signal succesfull receive when requiested.
-    eKraftMessageType_Ack_ID,
-    //Generic packet with parameters for updating RadioSettings.
-    eKraftMessageType_RadioSettings_ID,
-    //Used for sending strings.
-    eKraftMessageType_String_ID,
-    //Use numbers higher than this for custom IDs
-    eKraftMessageType_StandardEnd_ID
+    //General data.
+    eKraftMessageType_Data_ID,
+    //Telemetry data.
+    eKraftMessageType_Telemetry_ID,
+    //Telecommand data.
+    eKraftMessageType_Telecommand_ID,
+    //Datalink information e.g. ack, radio setting changes etc.
+    eKraftMessageType_Datalink_ID,
+    //This is to extend the enum
+    eKraftMessageType_End_ID
 };
 
 
 
 class KraftMessage_Interface {
-public:
+public: 
 
-    virtual uint32_t getDataTypeID() const = 0;
+    /**
+     * Message type is to differentiate between telemetry, telecommands, and general data.
+     * @returns the type of message. Not the type of data its holding.
+     */
+    virtual uint32_t getMessageType() const = 0;
 
+    /**
+     * Used to find out what datatype we are dealing with.
+     * @returns ID of datatype
+     */
+    virtual uint32_t getDataType() const = 0;
+
+    /**
+     * @returns size of data in bytes
+     */
     virtual uint32_t getDataSize() const = 0;
 
+    /**
+     * Places the contents of the message into the given buffer at the requested start position.
+     * @param dataBytes Pointer to the buffer the will receive the raw data.
+     * @param dataByteSize Max size of the given buffer to make sure given buffer is large enough.
+     * @param startByte Index of the starting position to place raw data. Defaults to 0.
+     * @returns true if successfull, false if something was not right.
+     */
     virtual bool getRawData(void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) const = 0;
 
+    /**
+     * Places the contents of the message into the given buffer at the requested start position.
+     * @param dataBytes Pointer to the buffer that containes the raw data.
+     * @param dataByteSize Size of the given buffer to make sure given data if correct.
+     * @param startByte Index of the starting position to take data from and increment from.
+     * @returns true if successfull, false if something was not right.
+     */
     virtual bool setRawData(const void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) = 0;
 
-protected:
+private:
 
     mutable uint32_t bufferIndex = 0;
     mutable uint32_t bufferMaxIndex;
     mutable void* bufferWritePointer = nullptr;
     const void* bufferReadPointer = nullptr;
+
+protected:
 
 
     /**
@@ -111,132 +144,151 @@ protected:
 };
 
 
+/**
+ * Base class to simplify creating Data messages. This automatically implements getMessageType().
+ */
+class KraftMessageData_Abstract: public KraftMessage_Interface {
+public:
 
+    /**
+     * Message type is to differentiate between telemetry, telecommands, and general data.
+     * @returns the type of message. Not the type of data its holding.
+     */
+    virtual uint32_t getMessageType() const final override {return eKraftMessageType_t::eKraftMessageType_Data_ID;}
+
+};
+
+
+
+class KraftMessageTelemetry_Abstract: public KraftMessage_Interface {
+public:
+
+    /**
+     * Message type is to differentiate between telemetry, telecommands, and general data.
+     * @returns the type of message. Not the type of data its holding.
+     */
+    virtual uint32_t getMessageType() const final override {return eKraftMessageType_t::eKraftMessageType_Telemetry_ID;}
+
+};
+
+
+
+class KraftMessageTelecommand_Abstract: public KraftMessage_Interface {
+public:
+
+    /**
+     * Message type is to differentiate between telemetry, telecommands, and general data.
+     * @returns the type of message. Not the type of data its holding.
+     */
+    virtual uint32_t getMessageType() const final override {return eKraftMessageType_t::eKraftMessageType_Telecommand_ID;}
+
+};
+
+
+
+class KraftMessageDatalink_Abstract: public KraftMessage_Interface {
+public:
+
+    /**
+     * Message type is to differentiate between telemetry, telecommands, and general data.
+     * @returns the type of message. Not the type of data its holding.
+     */
+    virtual uint32_t getMessageType() const final override {return eKraftMessageType_t::eKraftMessageType_Datalink_ID;}
+
+};
+
+
+
+/**
+ * This can be used to copy any type of KraftMessage and store it.
+ */
 class KraftMessageContainer {
 public:
 
+    KraftMessageContainer() {}
+
+    /**
+     * @param message Reference to message.
+     */
     KraftMessageContainer(const KraftMessage_Interface& message) {
-        dataType_ = static_cast<eKraftMessageType_t>(message.getDataTypeID());
-        dataSize_ = message.getDataSize();
-        data_ = new uint8_t[dataSize_];
-        message.getRawData(data_, dataSize_);
+        setMessage(message);
+    }
+
+    /**
+     * Used to store the data directly even if its an unknown data type.
+     * @param dataBytes Pointer to buffer containing data.
+     * @param numberBytes Exact number of raw bytes to contain.
+     * @param messageTypeID ID of message type.
+     * @param dataTypeID ID of data type.
+     */
+    KraftMessageContainer(const uint8_t* dataBytes, uint32_t numberBytes, uint32_t messageTypeID, uint32_t dataTypeID) {
+        setMessage(dataBytes, numberBytes, dataTypeID, messageTypeID);
     }
 
     ~KraftMessageContainer() {delete[] data_;}
 
-    const eKraftMessageType_t& getMessageID() const {return dataType_;}
+    /**
+     * @param message Reference to message.
+     * @returns true if successfull.
+     */
+    inline bool setMessage(const KraftMessage_Interface& message) {
+        delete[] data_;
+        dataType_ = message.getDataType();
+        dataSize_ = message.getDataSize();
+        messageType_ = message.getMessageType();
+        data_ = new uint8_t[dataSize_];
+        if (message.getRawData(data_, dataSize_)) {
+            isValid_ = true;
+            return true;
+        }
+        isValid_ = false;
+        return false;
 
-    bool getMessage(KraftMessage_Interface& message) const {
+    }
+
+    /**
+     * Used to store the data directly even if its an unknown data type.
+     * @param dataBytes Pointer to buffer containing data.
+     * @param numberBytes Exact number of raw bytes to contain.
+     * @param messageTypeID ID of message type.
+     * @param dataTypeID ID of data type.
+     * @returns true if successfull.
+     */
+    inline bool setMessage(const uint8_t* dataBytes, const uint32_t& numberBytes, const uint32_t& dataTypeID, const uint32_t& messageTypeID) {
+        delete[] data_;
+        dataType_ = dataTypeID;
+        dataSize_ = numberBytes;
+        messageType_ = messageTypeID;
+        data_ = new uint8_t[dataSize_];
+        for (uint32_t i = 0; i < dataSize_;i++) data_[i] = dataBytes[i];
+        isValid_ = true;
+        return true;
+    }
+
+    inline const uint32_t& getDataType() const {return dataType_;}
+
+    inline uint32_t getMessageType() const {return messageType_;}
+
+    inline const bool& isValid() const {return isValid_;}
+
+    inline bool getMessage(KraftMessage_Interface& message) const {
+        if (!isValid_) return false;
         return message.setRawData(data_, dataSize_);
+    }
+
+    inline KraftMessageContainer& operator= (const KraftMessageContainer& a) {
+        setMessage(a.data_, a.dataSize_, a.dataType_, a.messageType_);
+        return *this;
     }
 
 private:
 
     uint8_t* data_ = nullptr;
-    eKraftMessageType_t dataType_ = eKraftMessageType_t::eKraftMessageType_Invalid_ID;
+    uint32_t dataType_ = 0;
+    uint32_t messageType_ = eKraftMessageType_t::eKraftMessageType_Invalid_ID;
     uint8_t dataSize_ = 0;
 
-};
-
-
-
-class KraftMessageHeartbeat final: public KraftMessage_Interface {
-public:
-
-    uint32_t getDataTypeID() const {return eKraftMessageType_t::eKraftMessageType_Heartbeat_ID;}
-
-    uint32_t getDataSize() const {return 0;};
-
-    bool getRawData(void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) const {
-        return true;
-    }
-
-    bool setRawData(const void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) {
-        return true;
-    }
-
-};
-
-
-
-class KraftMessageACK final: public KraftMessage_Interface {
-public:
-
-    uint32_t getDataTypeID() const {return eKraftMessageType_t::eKraftMessageType_Ack_ID;}
-
-    uint32_t getDataSize() const {return 0;};
-
-    bool getRawData(void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) const {
-        return true;
-    }
-
-    bool setRawData(const void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) {
-        return true;
-    }
-
-};
-
-
-
-class KraftMessageStringPacket final: public KraftMessage_Interface {
-public:
-
-    KraftMessageStringPacket() {}
-
-    KraftMessageStringPacket(const char string[]) {
-        setString(string);
-    }
-
-    ~KraftMessageStringPacket() {
-        delete[] stringPointer;
-    }
-
-    uint32_t getDataTypeID() const {return eKraftMessageType_t::eKraftMessageType_String_ID;}
-
-    uint32_t getDataSize() const {return sizeStringPointer;};
-
-    uint32_t getStringLength() const {return sizeStringPointer;}
-
-    bool getString(char string[], const uint32_t &sizeStringDest) const {
-
-        if (sizeStringDest < sizeStringPointer || stringPointer == nullptr) return false;
-
-        memcpy(string, stringPointer, sizeStringPointer);
-
-        return true;
-
-    }
-
-    void setString(const char string[]) {
-        
-        if (stringPointer != nullptr) delete[] stringPointer;
-
-        sizeStringPointer = strlen(string)+1;
-        stringPointer = new char[sizeStringPointer];
-
-        memcpy(stringPointer, string, sizeStringPointer);
-
-    }
-
-    bool getRawData(void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) const {
-
-        return getString((char*)dataBytes+startByte, dataByteSize);
-
-    }
-
-    bool setRawData(const void* dataBytes, const uint32_t &dataByteSize, const uint32_t &startByte = 0) {
-
-        setString((char*)(dataBytes)+startByte);
-
-        return true;
-
-    }
-
-
-private:
-
-    char* stringPointer = nullptr;
-    uint32_t sizeStringPointer = 0;
+    bool isValid_ = false;
 
 };
 

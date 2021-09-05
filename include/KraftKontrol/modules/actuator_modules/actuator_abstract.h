db8eeea775dbb4356be8456dc9363552dbab3b00
Chris <chrissteffen98@gmail.com>
2021-09-05 10:53:25 +0200

    Added actuator and system model abstracts

---
+++ include/KraftKontrol/modules/actuator_modules/actuator_abstract.h
@@ -0,0 +1,81 @@
+#ifndef ACTUATOR_ABSTRACT_H
+#define ACTUATOR_ABSTRACT_H
+
+
+
+#include "Arduino.h"
+
+#include "KraftKontrol/data_containers/dynamic_data.h"
+
+#include "KraftKontrol/modules/module_abstract.h"
+
+#include "KraftKontrol/utils/list.h"
+
+#include "KraftKontrol/platforms/platform_hal.h"
+
+
+
+/**
+ * Class to be inhereted from. 
+ * Supports calculating total system force and torque for state estimation and simulation.
+ * 
+ */
+class Actuator_Abstract {
+private:
+
+    ///List of all actuators that exist.
+    static List<Actuator_Abstract*> actuatorList_;
+
+    ///Dummy dynamic data for returning 0 when not inhereted by subclass.
+    const DynamicData dummyData;
+
+protected:
+
+    /**
+     * Constructor called by subclasses-
+     * @param addToList If true then the actuator will be added to list of all actuators and included in calculations.
+     */
+    Actuator_Abstract(bool addToList = false) {
+        if (addToList) actuatorList_.append(this);
+    }
+
+public:
+    
+    /**
+     * Virtual destructor
+     * Removes actuator from list
+     */
+    virtual ~Actuator_Abstract() {
+        actuatorList_.removeAllEqual(this);
+    }
+
+    /**
+     * Calculates the actuators force and torqe imparted on system.
+     * @returns actuator resulting dynamics.
+     */
+    virtual const DynamicData& getActuatorDynamicData() const {return dummyData;}; 
+
+    /**
+     * Calculates total resulting system dynamics from all actuators.
+     * @returns total system force.
+     */
+    static DynamicData getSumDynamics() {
+
+        DynamicData totalDynamics;
+
+        for (uint32_t i = 0; i < actuatorList_.getNumItems(); i++) {
+
+            totalDynamics += actuatorList_[i]->getActuatorDynamicData();
+
+        }
+
+        return totalDynamics;
+
+    }
+
+    
+};
+
+
+
+#endif

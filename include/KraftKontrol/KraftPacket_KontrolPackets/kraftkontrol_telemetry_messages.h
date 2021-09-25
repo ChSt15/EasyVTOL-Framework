5a6b7a18750c011b4bfc5c138f21100235449759
Chris <chrissteffen98@gmail.com>
2021-09-25 14:18:00 +0200

    Added gnss telemetry message

--- include/KraftKontrol/KraftPacket_KontrolPackets/kraftkontrol_telemetry_messages.h
+++ include/KraftKontrol/KraftPacket_KontrolPackets/kraftkontrol_telemetry_messages.h
@@ -163,4 +163,67 @@ public:
 
 
 
+class TelemetryMessageGNSSData: public KraftMessage_Interface {
+public:
+
+    TelemetryMessageGNSSData() {}
+
+    TelemetryMessageGNSSData(double latitude, double longitude, float height, uint8_t numSats) {
+        lon_ = longitude;
+        lat_ = latitude;
+        height_ = height;
+        numSats_ = numSats;
+    }
+
+    uint32_t getDataType() const final override {return eMessageTypeTelemetry_t::eMessageTypeTelemetry_GNSSData;}
+
+    uint32_t getMessageType() const final override {return eKraftMessageType_t::eKraftMessageType_Telemetry_ID;}
+
+    uint32_t getDataSize() const {return sizeof(lon_) + sizeof(lat_) + sizeof(numSats_) + sizeof(height_);}
+
+    double getLatitude() {return lat_;}
+    double getLongitude() {return lon_;}
+    float getHeight() {return height_;}
+    uint8_t getNumSats() {return numSats_;}
+
+    bool getRawData(void* dataBytes, uint32_t dataByteSize, uint32_t startByte = 0) const override {
+
+        if (dataByteSize != getDataSize()) return false;
+
+        startBufferWrite(dataBytes, startByte);
+        bufferWrite((uint8_t*)&lon_, sizeof(lon_));
+        bufferWrite((uint8_t*)&lat_, sizeof(lat_));
+        bufferWrite((uint8_t*)&height_, sizeof(height_));
+        bufferWrite((uint8_t*)&numSats_, sizeof(numSats_));
+        endBufferWrite();
+
+        return true;
+
+    }
+
+    bool setRawData(const void* dataBytes, uint32_t dataByteSize, uint32_t startByte = 0) override{
+
+        if (dataByteSize != getDataSize()) return false;
+
+        startBufferRead(dataBytes, startByte);
+        bufferRead((uint8_t*)&lon_, sizeof(lon_));
+        bufferRead((uint8_t*)&lat_, sizeof(lat_));
+        bufferRead((uint8_t*)&height_, sizeof(height_));
+        bufferRead((uint8_t*)&numSats_, sizeof(numSats_));
+        endBufferRead();
+
+        return true;
+
+    }
+
+private:
+
+    double lon_, lat_;
+    float height_;
+    uint8_t numSats_ = 0;
+
+};
+
+
+
 #endif 
